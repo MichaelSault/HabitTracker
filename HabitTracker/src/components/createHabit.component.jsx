40b22a8463e7273f7d44938e95a3354c34b13851
MichaelSault <michael_sault@hotmail.com>
2023-11-11 23:48:28 -0500

    added week-date option to the habit creator

--- HabitTracker/src/components/createHabit.component.jsx
+++ HabitTracker/src/components/createHabit.component.jsx
@@ -42,8 +42,8 @@ function HabitTracker() {
 
     return (
         <>
-            <div id='bodyTest' style={{width:"100%", margin:"auto auto", textAlign:"center"}}>
-                
+            <h3>Add a New Habit</h3>
+            <div id='bodyTest' style={{width:"97%", margin:"auto auto", textAlign:"center"}}>
                 <Form>
                     <Form.Group>
                         <FloatingLabel
@@ -76,8 +76,59 @@ function HabitTracker() {
                             className="mb-3"
                         />
                         </FloatingLabel>
+
+                        <Form.Check
+                            inline
+                            label="S"
+                            name="group1"
+                            type='checkbox'
+                            id={`sunday`}
+                        />
+                        <Form.Check
+                            inline
+                            label="M"
+                            name="group1"
+                            type='checkbox'
+                            id={`monday`}
+                        />
+                        <Form.Check
+                            inline
+                            label="T"
+                            name="group1"
+                            type='checkbox'
+                            id={`tuesday`}
+                        />
+                        <Form.Check
+                            inline
+                            label="W"
+                            name="group1"
+                            type='checkbox'
+                            id={`wednesday`}
+                        />
+                        <Form.Check
+                            inline
+                            label="Th"
+                            name="group1"
+                            type='checkbox'
+                            id={`thursday`}
+                        />
+                        <Form.Check
+                            inline
+                            label="F"
+                            name="group1"
+                            type='checkbox'
+                            id={`friday`}
+                        />
+                        <Form.Check
+                            inline
+                            label="S"
+                            name="group1"
+                            type='checkbox'
+                            id="saturday"
+                        />
+
                     </Form.Group>
-                    <Button variant="outline-dark" style={{width:"100%", marginBottom:'1rem'}} onClick={handleClick}>Create Habit</Button>
+                    <Button variant="outline-dark" style={{width:"100%", marginBottom:'1rem'}} onClick={handleClick}>Create</Button>
                 </Form>
             </div>
         </>
